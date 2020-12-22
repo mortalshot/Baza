8b59b2e65b95aaeec8fa3122c207953fda7c6707
mortalshot <seregagl1996@gmail.com>
2020-12-22 18:13:13 +0300

    fix fix fix fix fix fix

--- #src/js/components/menu.js
+++ #src/js/components/menu.js
@@ -2,3 +2,41 @@ $('.header__burger').click(function (event) {
     $('.header__burger, #navbarSupportedContent').toggleClass('active');
     $('body').toggleClass('lock');
 })
+
+$(function () {
+    const section = $('.section'),
+        nav = $('.header .navbar'),
+        navHeight = nav.outerHeight(); // получаем высоту навигации 
+
+    // поворот экрана 
+    window.addEventListener('orientationchange', function () {
+        navHeight = nav.outerHeight();
+    }, false);
+
+    $(window).on('scroll', function () {
+        const position = $(this).scrollTop();
+
+        section.each(function () {
+            const top = $(this).offset().top - navHeight - 5,
+                bottom = top + $(this).outerHeight();
+
+            if (position >= top && position <= bottom) {
+                nav.find('.nav-link').removeClass('active');
+                section.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('.nav-link[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+
+    nav.find('.nav-link').on('click', function () {
+        const id = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - navHeight
+        }, 487);
+
+        return false;
+    });
+});
--- Baza/js/script.es5.js
+++ Baza/js/script.es5.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _readOnlyError(name) { throw new TypeError("\"" + name + "\" is read-only"); }
+
 $(document).ready(function () {
   function testWebP(callback) {
     var webP = new Image();
@@ -21,6 +23,37 @@ $(document).ready(function () {
   $('.header__burger').click(function (event) {
     $('.header__burger, #navbarSupportedContent').toggleClass('active');
     $('body').toggleClass('lock');
+  });
+  $(function () {
+    var section = $('.section'),
+        nav = $('.header .navbar'),
+        navHeight = nav.outerHeight(); // получаем высоту навигации 
+    // поворот экрана 
+
+    window.addEventListener('orientationchange', function () {
+      navHeight = (_readOnlyError("navHeight"), nav.outerHeight());
+    }, false);
+    $(window).on('scroll', function () {
+      var position = $(this).scrollTop();
+      section.each(function () {
+        var top = $(this).offset().top - navHeight - 5,
+            bottom = top + $(this).outerHeight();
+
+        if (position >= top && position <= bottom) {
+          nav.find('.nav-link').removeClass('active');
+          section.removeClass('active');
+          $(this).addClass('active');
+          nav.find('.nav-link[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+      });
+    });
+    nav.find('.nav-link').on('click', function () {
+      var id = $(this).attr('href');
+      $('html, body').animate({
+        scrollTop: $(id).offset().top - navHeight
+      }, 487);
+      return false;
+    });
   });
   var phone = $('#phone');
   var carrotLabel = $('#carrotLabel');
