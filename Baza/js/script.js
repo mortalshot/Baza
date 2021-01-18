b651bbb915487f9b2d6493e8e21c72320d0381b8
mortalshot <seregagl1996@gmail.com>
2021-01-18 15:14:46 +0300

    delivery__history

--- Baza/js/script.js
+++ Baza/js/script.js
@@ -842,11 +842,12 @@ if (slider) {
 
 ;
 
-    let deliveryTimelineItemHeight = $('.delivery-timeline__item').outerHeight(true);
-    let deliveryTimelineBtn = $('.delivery-timeline__btn');
-
-    deliveryTimelineBtn.click(function (e) {
+    $('.delivery-timeline__btn').click(function (e) {
         e.preventDefault();
         $('.delivery-timeline__wrapper').toggleClass('active');
     });
+
+    if ($('.delivery-history__item')) {
+        $('.delivery-history__timeline').css({ 'padding-top': $('.delivery-history__item').outerHeight() / 2, 'padding-bottom': $('.delivery-history__item').outerHeight() / 2 });
+    }
 })
